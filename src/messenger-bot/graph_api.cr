33b114b294fa76fbc50cd1cc3f9c16f063946c8b
Marius Mathiesen <marius@shortcut.no>
2016-09-29 09:24:07 +0200

    Handle message send exceptions, speculative deferred send

--- src/messenger-bot/graph_api.cr
+++ src/messenger-bot/graph_api.cr
@@ -14,8 +14,25 @@ module Messenger::Bot
       response = HTTP::Client.post("https://graph.facebook.com/v#{@api_version}/me/messages?access_token=#{@access_token}",
         body: message,
         headers: HTTP::Headers{"Content-Type" => "application/json"})
-      result = GraphResponseMessage.from_json(response.body)
-      return result.recipient_id, result.message_id
+      begin
+        result = GraphResponseMessage.from_json(response.body)
+        return result.recipient_id, result.message_id
+      rescue ex
+        puts ex.message
+        return 0, 0
+      end
+    end
+
+    # Notify the user that we're working on a response, and then deliver the
+    # final message once it's ready
+    # This is still work in progress, reading up on https://crystal-lang.org/docs/guides/concurrency.html
+    def deferred_send(recipient_id : Int64, channel : Channel(GraphAPI))
+      spawn do
+        message = Builder.new(recipient_id)
+                  .build_wait_message
+        deliver_message(message)
+        channel.send(self)
+      end
     end
   end
 end
